f0665914b91871b4b9b0b12e6c17cbf889e9b194
juzzms <jusnainimuslimin@gmail.com>
2025-06-11 13:33:07 +0800

    make first API call

---
+++ weather_pipeline.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = "Kuala Lumpur"
+params = {
+    "q": city,
+    "appid": API_KEY,
+    "units": "metric"
+}
+
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+
+print(data)
+
+
